dbd6923576eca8b5321d13affc14bb46357d53ae
Trí Võ <bananapixel221104@gmail.com>
2026-08-27 11:40:06 +0700

    add comment explain

--- app/services/user_roles/synchronizer.rb
+++ app/services/user_roles/synchronizer.rb
@@ -1,7 +1,7 @@
 class UserRoles::Synchronizer
   def initialize(user:, role_ids:)
     @user = user
-    @role_ids = Array(role_ids).map(&:to_i)
+    @role_ids = Array(role_ids).map(&:to_i) # shorten of .map { |role_id| role_id.to_i }
   end
 
   def call
